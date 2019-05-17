ce3c1ca0be47037be6dfd8aefbe9683b44850565
Refa Annisa <rans5021@gmail.com>
2019-05-18 04:33:28 +0700

    tugas 5

---
+++ TugasAnalgo5/matrixAdjacency.cpp
@@ -0,0 +1,29 @@
+/*
+Nama Program    : matrixAdjacency.cpp
+Nama            : Refa Annisatul Ilma
+NPM             : 140810170060
+Tanggal Buat    :
+Deskripsi       :
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int matriks[8][8] = {
+	{0,1,1,0,0,0,0,0},
+	{1,0,1,1,1,0,0,0},
+	{1,1,0,0,1,0,1,1},
+	{0,1,0,1,1,0,0,0},
+	{0,1,1,1,0,1,0,0},
+	{0,0,0,0,1,0,0,0},
+	{0,0,1,0,0,0,0,1},
+	{0,0,1,0,0,0,1,0}
+	};
+	for(int i=0;i<8;i++){
+		for(int j=0; j<8;j++){
+			cout<<matriks[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
